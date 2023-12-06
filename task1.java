3a4eea0ff38e62e3640502683a12ade5134b39cf
Leonid Maslov <mlenkei@mail.ru>
2023-12-06 10:29:09 +0500

    Update task1.java
    Изменения по заданию

--- task1.java
+++ task1.java
@@ -1,13 +1,1 @@
 public class Application {
-    public void init() {
-    }
-
-    public void run() {
-    }
-
-    public static void main(String[] args) {
-        Application application = new Application();
-        application.init();
-        application.run();
-    }
-}
